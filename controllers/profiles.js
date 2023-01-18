e6103f6a3600763d293baf621928f9fc96e934a2
Austin Friesorger <aafrieso@svsu.edu>
2023-01-17 22:44:39 -0500

    update profiles

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -1,17 +1,68 @@
 import { Profile } from "../models/profile.js"
+import { Stormie } from "../models/stormie.js"
 
-function generateProfile(req, res) {
+function index(req, res) {
   Profile.find({})
   .then(profiles => {
-    res.render("profiles/new", {
-      profiles: profiles
+    res.render('profiles/index', {
+      profiles,
+      title: 'All Users',
+  })
+})
+.catch(error => {
+  console.log(error)
+  res.redirect('/songs')
+})
+}
+
+function show(req, res) {
+  req.body.people = !!req.body.people
+  Profile.findById(req.params.id)
+  .then(profile => {
+    res.render('profiles/show', {
+      profile,
+      title: 'Profile Details'
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/')
+  })
+}
+
+
+function edit(req, res) {
+  Profile.findById(req.params.id)
+  .then(profile => {
+    res.render('profiles/edit', {
+      profile,
+      title: "Edit User"
     })
   })
   .catch(err => {
-    res.redirect("/profiles")
+    console.log(err)
+    res.redirect('/profiles')
+  })
+}
+
+function update(req, res) {
+  Profile.findById(req.params.id)
+  .then(profile => {
+    req.body.people = !!req.body.people
+    profile.updateOne(req.body)
+    .then(() => {
+      res.redirect(`/profiles/${profile._id}`)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/profiles')
   })
 }
 
 export {
-  generateProfile as new,
+  index,
+  show,
+  edit,
+  update
 }
